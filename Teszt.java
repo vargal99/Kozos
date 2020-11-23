010663529e6becc95df7acd5df9ccdee9c4b3c65
vargal99 <74590627+vargal99@users.noreply.github.com>
2020-11-23 15:33:07 +0100

    Add files via upload

---
+++ Teszt.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lacika
+ */
+public class Teszt {
+    /**
+     * @param args the command line arguments
+     */
+    
+     public static int kivonas(int x, int y){
+        int eredmeny=x-y;
+        return eredmeny;
+}
+     public static void main(String[] args) {
+        // TODO code application logic here
+        int a=3;
+        int b=2;
+        System.out.println(kivonas(a,b));
+    }
+}
